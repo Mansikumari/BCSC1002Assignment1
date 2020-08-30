da5980c9ec4db81bbc6c3a9077b75b551b2e2b0f
91797 <mansikumari7970@gmail.com>
2020-08-30 23:37:10 +0530

    add getter and setter method.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -10,7 +10,58 @@ public class Student {
     public String firstName;
     public String middleName;
     public System lastName;
+    public long universityRollNumber;
+    public int numberOfBooksIssuedByTheStudent;
+    private Book[] store;
+
+    public Book[] getStore(){
+        return store.clone(){
+
+        }
+        public void Setstore(Book[] store){
+            this.store=store;
+
+        }
+    }
 
 
 
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public System getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(System lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfBooksIssuedByTheStudent() {
+        return numberOfBooksIssuedByTheStudent;
+    }
+
+    public void setNumberOfBooksIssuedByTheStudent(int numberOfBooksIssuedByTheStudent) {
+        this.numberOfBooksIssuedByTheStudent = numberOfBooksIssuedByTheStudent;
+    }
 }
